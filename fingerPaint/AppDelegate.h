b22fd6fd669b4387f996ca6eec29aaeb7bfab450
Navaneethan Sivabalaviknarajah <niruns@gmail.com>
2015-07-27 15:32:57 -0400

    Initial Commit

---
+++ fingerPaint/AppDelegate.h
@@ -0,0 +1,17 @@
+//
+//  AppDelegate.h
+//  fingerPaint
+//
+//  Created by Navaneethan Sivabalaviknarajah on 2015-07-27.
+//  Copyright (c) 2015 Navaneethan Sivabalaviknarajah. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface AppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+
+@end
+
---
+++ fingerPaint/ViewController.h
@@ -0,0 +1,15 @@
+//
+//  ViewController.h
+//  fingerPaint
+//
+//  Created by Navaneethan Sivabalaviknarajah on 2015-07-27.
+//  Copyright (c) 2015 Navaneethan Sivabalaviknarajah. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface ViewController : UIViewController
+
+
+@end
+
